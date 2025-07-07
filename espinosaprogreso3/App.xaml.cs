4128fec2069d409e2a6d64f2fcda612589dc76ef
Johann Calva <johanncalvacadme@gmail.com>
2025-07-07 07:13:44 -0500

    Add project files.

---
+++ espinosaprogreso3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace espinosaprogreso3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
